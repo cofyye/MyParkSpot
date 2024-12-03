9f9ae5edbf740d7d576b11ce16ae52b9b52870be
aleksa <aleksa.ilic.dv@outlook.com>
2024-12-03 21:32:50 +0100

    initial commit

---
+++ MyParkSpot/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path');
+const homeRoutes = require('./src/routes/homeRoutes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', homeRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
