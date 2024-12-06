278649766cb799962b36ae0c68f308ba3054d138
aleksa <aleksa.ilic.dv@outlook.com>
2024-12-06 01:07:23 +0100

    redesigned home page, header and footer, configured tailwind theme

--- MyParkSpot/tailwind.config.js
+++ MyParkSpot/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{html,ejs}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#01010d',
+        secondary: '#606060',
+        light: '#f3f3f3',
+        background: '#ffffff',
+        hover: '#2e2e2e',
+        accent1: '#829aaa',
+      },
+      fontFamily: {
+        sans: ['Helvetica Neue', 'Arial', 'sans-serif'],
+        serif: ['Georgia', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
